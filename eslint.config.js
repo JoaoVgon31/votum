a9957ad1e0a54d4f777c4ae04af8de15f42e144a
João Vitor Gonçalves Oliveira <joaovitorgoliveira31@gmail.com>
2026-09-20 12:52:01 -0300

    Adiciona pasta coverage como ignorada pelo eslint

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,7 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
-  globalIgnores(['dist', 'src/routeTree.gen.ts']),
+  globalIgnores(['dist', 'src/routeTree.gen.ts', 'coverage']),
   {
     files: ['**/*.{ts,tsx}'],
     plugins: {
